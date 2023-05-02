27ac6b284e700b041ca5d5eabb0dab880feb4e97
ihsajjad <sajjadmeal@gmail.com>
2023-05-02 21:05:10 +0600

    added login with google and github btn in login and register page. some styles were changed

--- src/user/login/Login.jsx
+++ src/user/login/Login.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { FaGithub, FaGoogle } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div className="hero min-h-screen bg-base-200">
-            <form className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+        <div className="min-h-screen bg-base-200 w-full py-12 px-3">
+            <form className="rounded-lg p-5 lg:w-1/3 w-full mx-auto shadow-2xl bg-base-100 my-10">
                 <h2 className='text-2xl font-bold text-center my-5'>Please Login</h2>
                 <div className="card-body">
                     <div className="form-control">
@@ -26,6 +27,16 @@ const Login = () => {
                         <button className="btn btn-primary">Login</button>
                     </div>
                     <p>New at Cook's Compass? Please <Link to='/register' class="link link-primary">Register</Link></p>
+                </div>
+                <div className='text-center'>
+                    <h3>Login with</h3>
+                    <hr className='border border-slate-300 my-3 w-3/4 mx-auto' />
+                    <div className='flex items-center justify-center gap-5 mb-4'>
+                        <div className="btn btn-primary">
+                            <FaGoogle className='text-2xl mr-3'/><span> Google</span>
+                        </div>
+                        <div className="btn btn-primary"><FaGithub  className='text-2xl mr-3'/><span>Github</span></div>
+                    </div>
                 </div>
             </form>
         </div>
--- src/user/register/Register.jsx
+++ src/user/register/Register.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FaGithub, FaGoogle } from 'react-icons/fa';
+
 
 const Register = () => {
     return (
-        <div className="hero min-h-screen bg-base-200 w-full">
-            <form className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 my-10">
+        <div className="min-h-screen bg-base-200 w-full py-12 px-3">
+            <form className="rounded-lg p-5 lg:w-1/3 w-full mx-auto shadow-2xl bg-base-100 my-10">
                 <h2 className='text-2xl font-bold text-center my-5'>Please Register</h2>
                 <div className="card-body">
                     <div className="form-control">
@@ -44,6 +46,16 @@ const Register = () => {
                         <button className="btn btn-primary">Register</button>
                     </div>
                     <p>Already have an Account? Please <Link to='/login' class="link link-primary">Login</Link></p>
+                </div>
+                <div className='text-center'>
+                    <h3>Login with</h3>
+                    <hr className='border border-slate-300 my-3 w-3/4 mx-auto' />
+                    <div className='flex items-center justify-center gap-5 mb-4'>
+                        <div className="btn btn-primary">
+                            <FaGoogle className='text-2xl mr-3'/><span> Google</span>
+                        </div>
+                        <div className="btn btn-primary"><FaGithub  className='text-2xl mr-3'/><span>Github</span></div>
+                    </div>
                 </div>
             </form>
         </div>
