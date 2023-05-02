9ed1da88b7fe1052574549af8be66916f42b9180
ihsajjad <sajjadmeal@gmail.com>
2023-05-02 20:23:57 +0600

    added register page without functionality

--- src/user/register/Register.jsx
+++ src/user/register/Register.jsx
@@ -1,9 +1,51 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
-        <div>
-            <h2>Register please...</h2>
+        <div className="hero min-h-screen bg-base-200 w-full">
+            <form className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 my-10">
+                <h2 className='text-2xl font-bold text-center my-5'>Please Register</h2>
+                <div className="card-body">
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Name</span>
+                        </label>
+                        <input type="text" name='name' placeholder="Your Name" className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Photo URL</span>
+                        </label>
+                        <input type="text" name='photo' placeholder="Your Photo URL" className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="text" name='email' placeholder="Your Email" className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type="text" name='password' placeholder="Your Password" className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Confirm Password</span>
+                        </label>
+                        <input type="password" name='confirm' placeholder="Confirm Password" className="input input-bordered" />
+                        <label className="label">
+                            <p>error</p>
+                        </label>
+                    </div>
+                    <div className="form-control">
+                        <button className="btn btn-primary">Register</button>
+                    </div>
+                    <p>Already have an Account? Please <Link to='/login' class="link link-primary">Login</Link></p>
+                </div>
+            </form>
         </div>
     );
 };
