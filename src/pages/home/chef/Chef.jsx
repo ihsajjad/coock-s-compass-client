d101783810ba897579a8b442bd858c36a26b4456
ihsajjad <sajjadmeal@gmail.com>
2023-05-04 03:06:25 +0600

    install react lazy load and used on the chefs images

--- src/pages/home/chef/Chef.jsx
+++ src/pages/home/chef/Chef.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import { FaThumbsUp } from 'react-icons/fa';
+import LazyLoad from 'react-lazy-load';
 import { Link } from 'react-router-dom';
 
 const Chef = ({ chef }) => {
     const { chef_name, chef_picture, likes, years_of_experience, num_recipes, id } = chef;
 
-    
+
     return (
-        <div className='border-2 border-amber-300 rounded-lg p-5'>
-            <img className='rounded-t-lg mx-auto' src={chef_picture} style={{ height: '336px', width: '336px' }} alt="" />
+        <div className='border-2 border-amber-300 rounded-lg p-5 text-slate-500'>
+            <LazyLoad height={336}>
+                <img className='rounded-t-lg mx-auto' src={chef_picture} alt="" />
+            </LazyLoad>
             <div className='my-4'>
                 <h3 className='text-2xl font-semibold'>{chef_name}</h3>
                 <p>Years of Experience: {years_of_experience}</p>
                 <p>Recipes: {num_recipes}</p>
             </div>
-            <hr className='border border-amber-300 mb-3'/>
+            <hr className='border border-amber-300 mb-3' />
             <div className='flex items-center justify-between'>
-                <div className='flex justify-center items-center gap-2'><FaThumbsUp className='text-amber-400 text-xl'/> {likes}</div>
+                <div className='flex justify-center items-center gap-2'><FaThumbsUp className='text-amber-400 text-xl' /> {likes}</div>
                 <Link to={`/chefs/${id}`} className='custom-btn' chef={chef}>View Recipes</Link>
             </div>
         </div>
--- src/pages/home/product/Product.jsx
+++ src/pages/home/product/Product.jsx
@@ -6,7 +6,7 @@ const Product = ({ product }) => {
     console.log(product)
     return (
         <div className='border border-amber-300 rounded-lg p-3 space-y-3 shadow-lg'>
-            <img className='rounded-t-lg mx-auto my-4 h-40 w-60' src={image} alt="" />
+            <img className='rounded-t-lg mx-auto mb-4 h-40 w-60' src={image} alt="" />
             <h3 className='text-3xl font-semibold'>{name}</h3>
             <p><span className='font-bold'>Description: </span>{cookingMethod.slice(0,50)}</p>
             <p><span className='font-bold'>Price: </span> ${price}</p>
